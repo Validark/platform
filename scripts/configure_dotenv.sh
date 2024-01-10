544d110b31b7aa71b85138e8c1c582fe9f045353
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2024-01-10 16:58:32 +0100

    fix: get seed node cfg for rs-sdk configs

--- scripts/configure_dotenv.sh
+++ scripts/configure_dotenv.sh
@@ -17,13 +17,16 @@ WALLET_LIB_PATH="${PACKAGES_PATH}"/wallet-lib
 
 # Get configuration option from dashmate config for 1st node
 # Usage:
-#   get_config <option>
+#   get_config <node> <option>
+#
+# where <node> is either node number (1,2,3) or 'seed'
+# and <option> is a dot-separated path to the option
 #
 # Example:
-#   get_config core.rpc.port
+#   get_config seed core.rpc.port
 function get_config() {
     # We use `jq` because it's much faster than `yarn dashmate config get`
-    jq ".configs.${CONFIG}_1.$1" <"${HOME}"/.dashmate/config.json
+    jq ".configs.${CONFIG}_$1.$2" <"${HOME}"/.dashmate/config.json
 }
 
 touch "${LOGS_PATH}"/mint.log
@@ -35,7 +38,7 @@ cp "${DAPI_PATH}"/.env.example "${DAPI_PATH}"/.env
 FAUCET_ADDRESS=$(grep -m 1 "Address:" "${LOGS_PATH}"/mint.log | awk '{printf $3}')
 FAUCET_PRIVATE_KEY=$(grep -m 1 "Private key:" "${LOGS_PATH}"/mint.log | awk '{printf $4}')
 # TODO This will be removed from dashmate. Please use hardcoded ID from contract crates
-DPNS_CONTRACT_ID=$(get_config platform.dpns.contract.id)
+DPNS_CONTRACT_ID=$(get_config 1 platform.dpns.contract.id)
 
 SDK_ENV_FILE_PATH=${SDK_PATH}/.env
 rm -f "${SDK_ENV_FILE_PATH}"
@@ -65,10 +68,10 @@ NETWORK=regtest" >>"${WALLET_LIB_ENV_FILE_PATH}"
 
 # RS_SDK tests config
 
-CORE_RPC_PORT=$(get_config core.rpc.port)
-CORE_RPC_USER=$(get_config core.rpc.user)
-CORE_RPC_PASSWORD=$(get_config core.rpc.password)
-PLATFORM_RPC_PORT=$(get_config platform.dapi.envoy.http.port)
+CORE_RPC_PORT=$(get_config seed core.rpc.port)
+CORE_RPC_USER=$(get_config seed core.rpc.user)
+CORE_RPC_PASSWORD=$(get_config seed core.rpc.password)
+PLATFORM_RPC_PORT=$(get_config 1 platform.dapi.envoy.http.port)
 
 cat <<EOF >"${RS_SDK_PATH}"/tests/.env
 # Configuration of rs-sdk network tests
